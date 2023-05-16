c5894fda35c9838f2925cdcc27cd51ab99ccb7b3
ZukaGap <zurabigaprindashvili@gmail.com>
2023-05-17 01:31:55 +0400

    fix: home screen

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,5 +1,5 @@
-import React, {useCallback} from 'react';
-import {ActivityIndicator, Animated, View} from 'react-native';
+import React, {useCallback, useRef} from 'react';
+import {ActivityIndicator, ListRenderItem, View, Animated} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {useRecoilValue} from 'recoil';
 import {useNavigation} from '@react-navigation/native';
@@ -8,8 +8,8 @@ import {
   CollapsibleSubHeaderAnimator,
 } from 'react-navigation-collapsible';
 
-import {currenciesAtom} from '../../store/atom/getAtom';
-import {CurrencyBullet, Header} from '../../components';
+import {currenciesAtom} from 'store/atom/getAtom';
+import {CurrencyBullet, Header} from 'components';
 import {CurrenciesType} from 'config/Axios/getAPI';
 
 import getStyleObj from './style';
@@ -19,36 +19,40 @@ const HomeScreen: React.FC = () => {
   const insets = useSafeAreaInsets();
   const styles = getStyleObj(insets);
   const {navigate} = useNavigation();
+  const viewConfig = useRef({viewAreaCoveragePercentThreshold: 50}).current;
 
   const {data} = useRecoilValue(currenciesAtom);
 
   const {onScroll, containerPaddingTop, scrollIndicatorInsetTop, translateY} =
     useCollapsibleSubHeader();
 
-  const renderItem = useCallback(
-    ({item}: {item: CurrenciesType; index: number}) => {
-      return (
-        <CurrencyBullet
-          {...item}
-          onPress={() => {
-            navigate('detailScreen', item);
-          }}
-        />
-      );
-    },
-    [navigate],
-  );
+  const keyExtractor = useCallback((item: CurrenciesType) => item?.name, []);
+
+  const renderItem: ListRenderItem<CurrenciesType> = ({item}) => {
+    return (
+      <CurrencyBullet
+        {...item}
+        onPress={() => navigate('detailScreen', item)}
+      />
+    );
+  };
 
   return (
     <View style={styles.safeAreaWrapper}>
       <Animated.FlatList
         data={data}
-        onScroll={onScroll}
         renderItem={renderItem}
-        style={{paddingHorizontal: 16}}
-        contentContainerStyle={{paddingTop: containerPaddingTop}}
-        scrollIndicatorInsets={{top: scrollIndicatorInsetTop}}
+        keyExtractor={keyExtractor}
         showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingTop: containerPaddingTop,
+          paddingBottom: insets.bottom,
+        }}
+        scrollIndicatorInsets={{top: scrollIndicatorInsetTop}}
+        bounces={false}
+        scrollEventThrottle={16}
+        viewabilityConfig={viewConfig}
+        onScroll={onScroll}
         ListEmptyComponent={() => (
           <View>
             <ActivityIndicator size={'large'} color={colors.purple03} />
--- src/screens/HomeScreen/style.tsx
+++ src/screens/HomeScreen/style.tsx
@@ -1,17 +1,20 @@
-import {Platform, StyleSheet} from 'react-native';
+import {Dimensions, Platform, StyleSheet} from 'react-native';
 import {EdgeInsets} from 'react-native-safe-area-context';
 import {colors} from 'styles/colors';
 import {fonts} from 'styles/fonts';
 import {sizes} from 'styles/sizes';
+
+const {width, height} = Dimensions.get('window');
 
 const getStyleObj = (insets: EdgeInsets) => {
   return StyleSheet.create({
     safeAreaWrapper: {
       flex: 1,
       backgroundColor: '#ffffff',
-      // paddingHorizontal: 16,
+      paddingHorizontal: 16,
       ...Platform.select({
-        android: {paddingTop: insets.top, paddingBottom: insets.bottom},
+        // ios: {paddingTop: insets.top, paddingBottom: insets.bottom},
+        // android: {paddingTop: insets.top, paddingBottom: insets.bottom},
         default: {},
       }),
     },
@@ -24,12 +27,19 @@ const getStyleObj = (insets: EdgeInsets) => {
       marginBottom: sizes.s,
     },
     header: {
-      // paddingTop: insets.top,
-      paddingVertical: sizes.m,
+      ...Platform.select({
+        ios: {
+          paddingTop: insets.top,
+          paddingBottom: sizes.m,
+        },
+        android: {
+          paddingVertical: sizes.m,
+        },
+      }),
       paddingHorizontal: sizes.m,
       backgroundColor: 'rgba(255,255,255,0.9)',
       justifyContent: 'center',
-      width: '100%',
+      width: width,
     },
   });
 };
