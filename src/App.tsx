5b4a3ae0d2ccdb76c57035efdd085fa157e28753
ZukaGap <zurabigaprindashvili@gmail.com>
2023-07-25 20:27:21 +0400

    fix: crashes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 import Routes from 'navigation/Router';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <Routes />
-    </SafeAreaProvider>
-    // <Routes />
+    <GestureHandlerRootView style={{flex: 1}}>
+      <SafeAreaProvider>
+        <Routes />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
